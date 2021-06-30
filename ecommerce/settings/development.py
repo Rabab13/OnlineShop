e20dce57817658243944f5736910ceea7375f1f7
Rabab13 <rababrain@gmail.com>
2021-06-30 16:30:04 +0200

    initial commit

--- ecommerce/settings/development.py
+++ ecommerce/settings/development.py
@@ -50,3 +50,4 @@ DATABASES = {
 
 STRIPE_PUBLIC_KEY = config('STRIPE_TEST_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_TEST_SECRET_KEY')
+SECRET_KEY = '8lu*6g0lg)9z!ba+a$ehk)xt)x%rxgb$i1&amp;022shmi1jcgihb*'
